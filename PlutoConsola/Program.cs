7c2aa08868a54a923c737c40ad37d1dba977212b
Pedro368 <56154168+Pedro368@users.noreply.github.com>
2021-04-20 13:47:20 +0200

    Primer commit

---
+++ PlutoConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlutoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
